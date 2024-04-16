80c9613d2f6f577204c101bccdda105981968b36
elenaannat <elena.toenjes@web.de>
2024-04-16 12:14:49 +0200

    Changed label size on heatmap.

--- absa_results_visualization/absa_descriptives.py
+++ absa_results_visualization/absa_descriptives.py
@@ -19,12 +19,23 @@ metadata = pd.read_pickle(r'data/230627_cleaned_dataset.pkl')
 # Create a pivot table
 pivot_table = sents.pivot_table(index='ESG-Subcategory', columns='year', values='Net_Sentiment', aggfunc='mean')
 
+
 # Creating the heatmap
 plt.figure(figsize=(12, 10))  # Adjust size to ensure all categories are visible
-ax = sns.heatmap(pivot_table, annot=False, fmt=".2f", cmap='coolwarm')
-ax.set_title('Average Net Sentiment by ESG Subcategory Over Years')
-plt.xticks(rotation=45)  # Rotate the years for better visibility
-plt.yticks(rotation=0)   # Ensure ESG subcategory names are readable
+ax = sns.heatmap(pivot_table, annot=False, fmt=".2f", cmap='coolwarm')  # Turn off annotations
+
+# Enhance the title and axis labels with bold formatting
+ax.set_title('Average Net Sentiment by ESG Subcategory Over Years', fontsize=16, weight='bold')
+
+# Increase font size for xticks and yticks and make them bold
+plt.xticks(rotation=45, fontsize=12, weight='bold')  # Rotate the years for better visibility and bold font
+plt.yticks(rotation=0, fontsize=12, weight='bold')   # Ensure ESG subcategory names are readable and bold
+
+# Optionally increase colorbar label size and make them bold
+cbar = ax.collections[0].colorbar
+cbar.ax.tick_params(labelsize=12)
+cbar.ax.yaxis.label.set_weight('bold')
+
 
 plt.savefig(r'./results/ESG_Subcategories_Sentiment_Heatmap.pdf', format='pdf', bbox_inches='tight')  # Save as PDF
 
